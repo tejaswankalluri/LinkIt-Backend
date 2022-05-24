8cd79c15ab0fcaa2d97358f97d91680dda3a5b42
tejaswan kalluri <tejaswankalluri82522@gmail.com>
2022-05-24 16:23:25 +0530

    !NOT Imp Commit
    The file contains how to use mongoose

--- src/controllers/hello.controller.ts
+++ src/controllers/hello.controller.ts
@@ -1,10 +1,29 @@
 import { Request, Response } from 'express';
+// import Links, { ILinks } from '../models/links.model';
+// import { HydratedDocument } from 'mongoose';
 // schema
 import User from '../models/user.model';
 // validation
 import { createUserValidate } from '../validation/user.validation';
 // controllers
-const getHelloUser = (req: Request, res: Response) => {
+const getHelloUser = async (req: Request, res: Response) => {
+    // try {
+    //     const link: HydratedDocument<ILinks> = new Links({
+    //         // @ts-ignore
+    //         userId: '628a5105d7e399ba818dec99',
+    //         // userId: req.user._id,
+    //         user_uri: 'tejaswan1',
+    //         links: {
+    //             name: 'youtube',
+    //             url: 'www.youtube.com',
+    //         },
+    //     });
+    //     await link.save();
+    //     console.log(link);
+    // } catch (err) {
+    //     console.log(err);
+    //     return res.status(500).send();
+    // }
     // @ts-ignore
     return res.send({ name: req.user.email });
 };
