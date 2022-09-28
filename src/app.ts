f98f97c818940200ac93d1e52be636428b3c9799
tejaswan kalluri <tejaswankalluri82522@gmail.com>
2022-09-28 17:08:38 +0530

    added cors and helmet

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,14 @@ import express, { Request, Response } from 'express';
 const app = express();
 import morgan from 'morgan';
 import passport from 'passport';
+import cors from 'cors';
+import helmet from 'helmet';
 const PORT = process.env.PORT;
 
 // middleware
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
 app.use(morgan('dev'));
 app.use(passport.initialize());
 import './middleware/passport';
